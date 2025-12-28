7eafd3a8b24fd47620cd088dd77cdc18c10d9258
LightningSamko <146434381+LightningSamko@users.noreply.github.com>
2025-12-28 17:49:57 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const loadCommands = require('./handlers/commandHandler');
+const loadEvents = require('./handlers/eventHandler');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+loadCommands(client);
+loadEvents(client);
+
+client.login(process.env.TOKEN);
